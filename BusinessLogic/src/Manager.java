d60097077f2a9e6d3c3755c707bcae6c6721c76a
Spch03 <Spch03@wsh71010.training.spc.at>
2013-06-04 16:17:15 +0200

    xxxx

--- BusinessLogic/src/Manager.java
+++ BusinessLogic/src/Manager.java
@@ -2,6 +2,7 @@ public class Manager extends Mitarbeiter {
 
   // das ist ein blöder Kommentar
   // und der ist genauso blöd
+  // auch blöd
   public Manager(int maNr, String titel, String vorname, String nachname) throws IllegalArgumentException {
     super(maNr, titel, vorname, nachname);
   }
