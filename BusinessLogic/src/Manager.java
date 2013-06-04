afcdc18592dc077e975ee50f9d1d2a30c199fcd3
Spch03 <Spch03@wsh71010.training.spc.at>
2013-06-04 16:12:34 +0200

    Kommentar hinzu

--- BusinessLogic/src/Manager.java
+++ BusinessLogic/src/Manager.java
@@ -1,8 +1,8 @@
-
 public class Manager extends Mitarbeiter {
 
-	public Manager(int maNr, String titel, String vorname, String nachname)
-			throws IllegalArgumentException {
-		super(maNr, titel, vorname, nachname);
-	}
+  // das ist ein blöder Kommentar
+  // und der ist genauso blöd
+  public Manager(int maNr, String titel, String vorname, String nachname) throws IllegalArgumentException {
+    super(maNr, titel, vorname, nachname);
+  }
 }
