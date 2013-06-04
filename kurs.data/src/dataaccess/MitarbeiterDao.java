8efb34bc30f23ccfa06a7274cdd83c91df0d7087
Spch05 <Spch05@spc.at>
2013-06-04 16:00:54 +0200

    initial commit

---
+++ kurs.data/src/dataaccess/MitarbeiterDao.java
@@ -0,0 +1,34 @@
+package dataaccess;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Spch05
+ */
+public class MitarbeiterDao {
+
+  private static final String FILE = "S:\\Mitarbeiter.txt";
+
+  public List<Object> getAllObjects() throws FileNotFoundException, IOException, ClassNotFoundException {
+    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE))) {
+      return (List<Object>) ois.readObject();
+    }
+  }
+
+  public void saveAllObjects(List<Serializable> list) throws IOException {
+    if (list == null || list.size() < 1) { throw new IllegalArgumentException(); }
+    // try with resources (Java 7)
+    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE))) {
+      oos.writeObject(list);
+    }
+
+  }
+
+}
