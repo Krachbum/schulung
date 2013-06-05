25b6a3cc0e1ae7182b9f65d6814cfb944d8e1682
Spch05 <Spch05@spc.at>
2013-06-05 10:31:04 +0200

    locales speichern der daten

--- kurs.data/src/dataaccess/MitarbeiterDao.java
+++ kurs.data/src/dataaccess/MitarbeiterDao.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class MitarbeiterDao {
 
-  private static final String FILE = "S:\\Mitarbeiter.txt";
+  private static final String FILE = "C:\\tmp\\Mitarbeiter.txt";
 
   public List<Object> getAllObjects() throws FileNotFoundException, IOException, ClassNotFoundException {
     // bla
