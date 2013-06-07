c2d50587c2797704aac4b9844584d13bae6362f1
Spch05 <Spch05@spc.at>
2013-06-07 10:27:38 +0200

    wir warn schneller

--- BusinessLogic/src/at/gv/brz/training/krachbum/bom/Zeitverwaltung.java
+++ BusinessLogic/src/at/gv/brz/training/krachbum/bom/Zeitverwaltung.java
@@ -1,5 +1,6 @@
 package at.gv.brz.training.krachbum.bom;
 
+import java.sql.Date;
 import java.util.List;
 
 public class Zeitverwaltung {
@@ -10,10 +11,32 @@ public class Zeitverwaltung {
     //
   }
 
-  //
-  // public void bucheZeit(Mitarbeiter ma, Date von, Date bis, Projekt project) {
-  // //
-  // }
+  public void bucheZeit(Mitarbeiter ma, Date von, Date bis, Projekt project) {
+
+    boolean mafound = false;
+    Projekt mitMirbuchen = null;
+
+    for (Mitarbeiter m : lMitarbeiter) {
+      if (m.equals(ma)) {
+        mafound = true;
+        break;
+      }
+    }
+
+    for (Projekt p : lProjekte) {
+      if (p.equals(project)) {
+        mitMirbuchen = p;
+        break;
+      }
+    }
+
+    if (bis.before(von)) { throw new IllegalArgumentException("Von muss vor Bis liegen"); }
+
+    if (mafound && mitMirbuchen != null) {
+      mitMirbuchen.addArbeitszeit(new Arbeitszeit(von, bis));
+    }
+
+  }
 
   public List<Projekt> getlProjekte() {
     return lProjekte;
